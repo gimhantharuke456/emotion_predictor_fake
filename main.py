45a75f77c88a91c18fd221bf9ed105ef59a8a24f
gimhantharuke456 <gimhantharuke456@gmail.com>
2023-08-27 01:26:14 +0530

    ADD: asda

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 import datetime
 import tensorflow as tf
 import gc
-import uvicorn
+
 app = FastAPI()
 
 # Set up CORS to allow requests from all origins
@@ -13,18 +13,21 @@ origins = ["*"]
 app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True, allow_methods=["*"],
                    allow_headers=["*"])
 
-
 def analyze_emotion(filename):
+    # Manually configure TensorFlow memory growth
+    gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+    if gpu_devices:
+        for device in gpu_devices:
+            tf.config.experimental.set_memory_growth(device, True)
+    
     emotions = DeepFace.analyze(filename)
     emotion = emotions[0]['dominant_emotion']
     print(f"emotion is {emotion}")
     return emotion
 
-
 @app.get("/test")
 def test():
     return {"message": "helloworld"}
-
 
 @app.post("/predict")
 async def predict_image(request: dict):
@@ -33,7 +36,7 @@ async def predict_image(request: dict):
         response = requests.get(image_url)
         current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         filename = f"image_{current_time}.jpg"
-
+        
         if response.status_code == 200:
             with open(filename, "wb") as f:
                 f.write(response.content)
@@ -41,22 +44,22 @@ async def predict_image(request: dict):
         else:
             print("Failed to download image. Status code:", response.status_code)
             return {"error": "Failed to download image"}, 500
-
-        emotion = analyze_emotion(filename)
-
+        
+        with tf.device('/CPU:0'):
+            emotion = analyze_emotion(filename)
+        
         # Clear TensorFlow session
         DeepFace.close_session()
-
+        
         gc.collect()  # Perform garbage collection
-
+        
         # Remove the downloaded image
         import os
         os.remove(filename)
-
+        
         return {"emotion": emotion}
     except Exception as e:
         return {"error": f"Error while predicting emotion: {str(e)}"}, 500
 
-
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8003)
